19e4e05967bdeb4bcb9373563a891cb04b84bdb2
Kamrin Kennedy <kamrinkennedy@gmail.com>
2018-10-02 18:19:50 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+var
+// function sayHiToGrandma(string) {
+//   if (lowercase.toLowerCase
+// }
